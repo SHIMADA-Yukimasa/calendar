f4745740eeb700c3331dceb832f72f880ddff007
SHIMADA-Yukimasa <shimada@eleos.jp>
2025-10-09 15:25:52 +0900

    予定を追加

--- calendar.js
+++ calendar.js
@@ -1,5 +1,36 @@
 const weekName = ['sun', 'mon', 'tue', 'wed', "thu", "fri", "sat"];
 const japaneseWeekName = '日月火水木金土'.split('');
+const yotei = [
+    [1, "元旦～３日　新春安払い祈祷祭"],
+    [1, "３日　毘沙門天福祭"],
+    [1, "11日　初稲荷"],
+    [1, "17日　初観音ご縁日"],
+    [1, "17日　五穀豊穣祈願祭"],
+    [1, "20日　権現祭"],
+    [2, "３日　節分会（星祭り）"],
+    [2, "15日　涅槃会"],
+    [2, "18日　初薬師"],
+    [3, "８日　霊山寺 大護摩火渡り祭"],
+    [3, "20日　春の彼岸"],
+    [4, "29日～　宝物館公開期間"],
+    [4, "29日～　還誕祭"],
+    [4, "29日～　ちびっこ厄払い祭"],
+    [4, "８日　花まつり"],
+    [4, "11日～17日　春のお教会式"],
+    [5, "～6日　宝物館公開期間"],
+    [5, "～6日　還誕祭"],
+    [5, "～6日　ちびっこ厄払い祭"],
+    [6, "14日　のぼり旗奉納者合同祈願祭"],
+    [7, "17日　御本尊開扉法要（大般若600巻転読）"],
+    [7, "22日～24日　全山閉山日"],
+    [8, "８日　霊山寺 十二天供養祭"],
+    [8, "15日　施餓鬼法要"],
+    [8, "17日　第28回 清水寺萬燈会"],
+    [9, "23日　秋の彼岸会"],
+    [10, "18日　光明真言会（諷誦）"],
+    [11, "11日　清水稲荷大祭（もっこ祭り）"],
+    [12, "17日　大梵焼祭"]
+];
 
 export function createCalendar (year, month)  {
     // 全体のdiv要素
@@ -8,8 +39,22 @@ export function createCalendar (year, month)  {
 
     // 月名のヘッダー部分
     const monthName = document.createElement("div");
-    monthName.classList.add("month-name");
-    monthName.innerText = month;
+    const getumei = document.createElement("div");
+    const yoteiContainar = document.createElement("div");
+    monthName.classList.add('month-name-containar');
+    yoteiContainar.classList.add('yotei-containar');
+    getumei.classList.add("month-name");
+    getumei.innerText = month;
+    yotei.forEach(v => {
+         if (v[0] === month) {
+             const div = document.createElement('div');
+             div.classList.add('yotei');
+             div.innerText = v[1];
+             yoteiContainar.appendChild(div);
+         }
+    });
+    monthName.appendChild(getumei);
+    monthName.appendChild(yoteiContainar);
     result.appendChild(monthName);
 
     // 曜日部分
